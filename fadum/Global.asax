601c41f6bb3fec98e24689f5de23059abb2c0c39
admin <admin@DESKTOP-NNLVJHQ>
2017-04-30 17:29:01 -0400

    Add project files.

---
+++ fadum/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="fadum.MvcApplication" Language="C#" %>
---
+++ fadum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(fadum.Startup))]
+namespace fadum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
